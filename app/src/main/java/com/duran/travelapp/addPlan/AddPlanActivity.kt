5841333855df7d7bd2623d86a158f84dcf8cf39c
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-06-30 07:53:22 +0900

    AddPlan 버튼 클릭시 이동한 엑티비티 새성

---
+++ app/src/main/java/com/duran/travelapp/addPlan/AddPlanActivity.kt
@@ -0,0 +1,12 @@
+package com.duran.travelapp.addPlan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.duran.travelapp.R
+
+class AddPlanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_plan)
+    }
+}
