1edae52ff4be7cd60f2a44824900570e6f55dd68
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-06-30 08:22:27 +0900

    AddPlan버튼 클릭시 엑티비티 이동 구현

--- app/src/main/java/com/duran/travelapp/travelPlan/TravelPlanFragment.kt
+++ app/src/main/java/com/duran/travelapp/travelPlan/TravelPlanFragment.kt
@@ -1,14 +1,19 @@
 
 package com.duran.travelapp.travelPlan
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.duran.travelapp.R
+import com.duran.travelapp.addPlan.AddPlanActivity
+import com.duran.travelapp.databinding.FragmentTravelPlanBinding
 
 class TravelPlanFragment : Fragment() {
+
+    private lateinit var hBinding: FragmentTravelPlanBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +21,22 @@ class TravelPlanFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_travel_plan, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentTravelPlanBinding = FragmentTravelPlanBinding.bind(view)
+
+        hBinding = fragmentTravelPlanBinding
+
+        fragmentTravelPlanBinding.btnAddTravelPlan.setOnClickListener {
+            context?.let {
+                val intent = Intent(it, AddPlanActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
     }
 
 }
