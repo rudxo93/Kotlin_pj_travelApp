d3e568645a6b1849a26fe253b865fc5a4f9db175
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-06-30 22:04:13 +0900

    addPlan 뒤로가기 버튼 -> MainActivity 이동

--- app/src/main/java/com/duran/travelapp/addPlan/AddPlanActivity.kt
+++ app/src/main/java/com/duran/travelapp/addPlan/AddPlanActivity.kt
@@ -1,13 +1,24 @@
 package com.duran.travelapp.addPlan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import com.duran.travelapp.MainActivity
 import com.duran.travelapp.R
 
 class AddPlanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_plan)
+
+        val btn_back = findViewById<ImageView>(R.id.iv_top_bar_arrow_back)
+
+        btn_back.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 
 }
