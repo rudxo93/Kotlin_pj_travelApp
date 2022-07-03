0f04ec70d202fe9df59dd403662bb155c4aba47c
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-03 23:44:33 +0900

    AddPlan에 사용할 Dto 작성

--- app/src/main/java/com/duran/travelapp/dto/AddPlanDto.kt
+++ app/src/main/java/com/duran/travelapp/dto/AddPlanDto.kt
@@ -1,4 +1,16 @@
 package com.duran.travelapp.dto
 
-class AddPlanDto {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "addPlan")
+class AddPlanDto(
+    @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "sdate") val sdate: String,
+    @ColumnInfo(name = "edate") val edate: String
+): Serializable {
+
 }
