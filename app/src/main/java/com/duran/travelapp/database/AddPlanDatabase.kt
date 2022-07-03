4da65db0ecd706ff25e8ffb2e9be93a8aa6382c6
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-04 01:24:19 +0900

    AddPlan에 사용할 Database와 Repository 작성

--- app/src/main/java/com/duran/travelapp/database/AddPlanDatabase.kt
+++ app/src/main/java/com/duran/travelapp/database/AddPlanDatabase.kt
@@ -1,4 +1,11 @@
 package com.duran.travelapp.database
 
-class AddPlanDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.duran.travelapp.dao.AddPlanDao
+import com.duran.travelapp.dto.AddPlanDto
+
+@Database(entities = arrayOf(AddPlanDto::class), version = 1)
+abstract class AddPlanDatabase : RoomDatabase() {
+    abstract fun addPlanDao(): AddPlanDao
 }
--- app/src/main/java/com/duran/travelapp/repository/AddPlanRepository.kt
+++ app/src/main/java/com/duran/travelapp/repository/AddPlanRepository.kt
@@ -1,4 +1,46 @@
 package com.duran.travelapp.repository
 
-class AddPlanRepository {
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import com.duran.travelapp.database.AddPlanDatabase
+import com.duran.travelapp.dto.AddPlanDto
+
+private const val DATABASE_NAME = "addPlan-database.db"
+
+class AddPlanRepository private constructor(context: Context) {
+
+    private val database: AddPlanDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        AddPlanDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val addPlanDao = database.addPlanDao()
+
+    fun list() : LiveData<MutableList<AddPlanDto>> = addPlanDao.list()
+
+    fun getPlan(id: Long): AddPlanDto = addPlanDao.selectOne(id)
+
+    fun insert(dto: AddPlanDto) = addPlanDao.insert(dto)
+
+    suspend fun update(dto: AddPlanDto) = addPlanDao.update(dto)
+
+    fun delete(dto: AddPlanDto) = addPlanDao.delete(dto)
+
+    companion object {
+        private var INSTANCE: AddPlanRepository?= null
+
+        fun initialize(context: Context) {
+            if(INSTANCE == null) {
+                INSTANCE = AddPlanRepository(context)
+            }
+        }
+
+        fun get(): AddPlanRepository {
+            return INSTANCE ?:
+            throw IllegalStateException("AddPlanRepository must be initialized")
+        }
+    }
+
 }
