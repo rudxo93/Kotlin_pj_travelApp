679b00c0138d3a84416c5dc7844158ed10fa1e14
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-04 00:25:55 +0900

    AddPlan에 사용할 Dao 작성

--- app/src/main/java/com/duran/travelapp/dao/AddPlanDao.kt
+++ app/src/main/java/com/duran/travelapp/dao/AddPlanDao.kt
@@ -1,4 +1,25 @@
 package com.duran.travelapp.dao
 
-class AddPlanDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.duran.travelapp.dto.AddPlanDto
+
+@Dao
+interface AddPlanDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(dto: AddPlanDto)
+
+    @Query("select * from addPlanTable")
+    fun list(): LiveData<MutableList<AddPlanDto>>
+
+    @Query("select * from addPlanTable where id = (:id)")
+    fun selectOne(id: Long): AddPlanDto
+
+    @Update
+    suspend fun update(dto: AddPlanDto)
+
+    @Delete
+    fun delete(dto: AddPlanDto)
+
 }
--- app/src/main/java/com/duran/travelapp/dto/AddPlanDto.kt
+++ app/src/main/java/com/duran/travelapp/dto/AddPlanDto.kt
@@ -5,12 +5,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
 
-@Entity(tableName = "addPlan")
+@Entity(tableName = "addPlanTable")
 class AddPlanDto(
     @ColumnInfo(name = "id") @PrimaryKey(autoGenerate = true) var id: Long = 0,
     @ColumnInfo(name = "title") val title: String,
     @ColumnInfo(name = "sdate") val sdate: String,
     @ColumnInfo(name = "edate") val edate: String
-): Serializable {
+): Serializable { // intent에 객체를 담이 위해 상속받는다.
 
 }
