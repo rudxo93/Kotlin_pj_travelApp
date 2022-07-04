f2a74a96ffaf8829f410fe1f8dcfbb4c38d2436a
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-04 12:37:26 +0900

    AddPlan에 사용할 viewModel 작성

--- app/src/main/java/com/duran/travelapp/viewmodel/AddPlanViewModel.kt
+++ app/src/main/java/com/duran/travelapp/viewmodel/AddPlanViewModel.kt
@@ -1,4 +1,34 @@
 package com.duran.travelapp.viewmodel
 
-class AddPlanViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.duran.travelapp.dto.AddPlanDto
+import com.duran.travelapp.repository.AddPlanRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class AddPlanViewModel: ViewModel() {
+
+    val addPlanList: LiveData<MutableList<AddPlanDto>>
+
+    private val addPlanRepository: AddPlanRepository = AddPlanRepository.get()
+
+    init {
+        addPlanList = addPlanRepository.list()
+    }
+
+    fun getOne(id: Long) = addPlanRepository.getPlan(id)
+
+    fun insert(dto: AddPlanDto) = viewModelScope.launch(Dispatchers.IO) {
+        addPlanRepository.insert(dto)
+    }
+
+    fun update(dto: AddPlanDto) = viewModelScope.launch(Dispatchers.IO) {
+        addPlanRepository.update(dto)
+    }
+
+    fun delete(dto: AddPlanDto) = viewModelScope.launch(Dispatchers.IO) {
+        addPlanRepository.delete(dto)
+    }
 }
