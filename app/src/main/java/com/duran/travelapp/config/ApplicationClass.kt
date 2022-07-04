81756ce345bc05751f954a4644cfb6b5dd97482a
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-04 12:41:56 +0900

    applicationClass 작성(앱이 실행될 때 단 한번 실행되도록 하기 위해)

--- app/src/main/java/com/duran/travelapp/config/ApplicationClass.kt
+++ app/src/main/java/com/duran/travelapp/config/ApplicationClass.kt
@@ -1,4 +1,12 @@
 package com.duran.travelapp.config
 
-class ApplicationClass {
+import android.app.Application
+import com.duran.travelapp.repository.AddPlanRepository
+
+class ApplicationClass: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AddPlanRepository.initialize(this)
+    }
 }
