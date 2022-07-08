9b946dae2ad5049b038378a6c04011df883b62db
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-07-08 14:11:28 +0900

    + 버튼 클릭 시 여행제목, 여행 시작일과 종료일 DB에 저장하기 구현(DB에 저장 확인)

--- app/src/main/java/com/duran/travelapp/addPlan/AddPlanActivity.kt
+++ app/src/main/java/com/duran/travelapp/addPlan/AddPlanActivity.kt
@@ -9,10 +9,14 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.duran.travelapp.MainActivity
 import com.duran.travelapp.R
+import com.duran.travelapp.databinding.ActivityAddPlanBinding
+import com.duran.travelapp.dto.AddPlanDto
 import java.text.SimpleDateFormat
 import java.util.*
 
 class AddPlanActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAddPlanBinding
 
     private lateinit var tvStartDatePicker : TextView
     private lateinit var btnStartDatePicker : ImageView
@@ -25,14 +29,25 @@ class AddPlanActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_plan)
+        binding = ActivityAddPlanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val type = intent.getStringExtra("type")
+
+        if(type.equals("ADD")){
+            binding.btnAddPlan.text = "일정 추가하기"
+        } else {
+            binding.btnAddPlan.text = "일정 수정하기"
+        }
 
         initBackBtn()
         initStartDatePicker()
         initEndDatePicker()
+        initBtnAddPlan()
 
     }
 
+    //  여행 시작일 DatePicker 클릭
     private fun initStartDatePicker() {
         tvStartDatePicker = findViewById(R.id.plan_tv_start_date)
         btnStartDatePicker = findViewById(R.id.plan_iv_start_dataPicker)
@@ -49,12 +64,14 @@ class AddPlanActivity : AppCompatActivity() {
         }
     }
 
+    //  여행 시작일 DatePicker 날짜 넣기
     private fun updateStartDateLable(sCalendar: Calendar) {
         val myFormat = "yyyy/MM/dd"
         val sdf = SimpleDateFormat(myFormat, Locale.UK)
         tvStartDatePicker.setText(sdf.format(sCalendar.time))
     }
 
+    //  여행 종료일 DatePicker 클릭
     private fun initEndDatePicker() {
         tvEndDatePicker = findViewById(R.id.plan_tv_end_date)
         btnEndDatePicker = findViewById(R.id.plan_iv_end_dataPicker)
@@ -71,19 +88,47 @@ class AddPlanActivity : AppCompatActivity() {
         }
     }
 
+    //  여행 종료일 DatePicker 날짜 넣기
     private fun updateEndDateLable(eCalendar: Calendar) {
         val myFormat = "yyyy/MM/dd"
         val sdf = SimpleDateFormat(myFormat, Locale.UK)
         tvEndDatePicker.setText(sdf.format(eCalendar.time))
     }
 
-    private fun initBackBtn() {
+    //  뒤로가기 버튼 클릭 시 이전 엑티비티로 돌아가기기
+   private fun initBackBtn() {
         val btn_back = findViewById<ImageView>(R.id.iv_top_bar_arrow_back)
 
         btn_back.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+   }
+
+    private fun initBtnAddPlan() {
+
+        val type = intent.getStringExtra("type")
+
+        binding.btnAddPlan.setOnClickListener {
+            val title = binding.planEtTitle.text.toString()
+            val sDate = binding.planTvStartDate.text.toString()
+            val eDate = binding.planTvEndDate.text.toString()
+
+            if(type.equals("ADD")){
+                if(title.isNotEmpty() && sDate.isNotEmpty() && eDate.isNotEmpty()){
+                    val addPlan = AddPlanDto(0, title, sDate, eDate)
+                    val intent = Intent().apply {
+                        putExtra("addPlan", addPlan)
+                        putExtra("flag", 0)
+                    }
+                    setResult(RESULT_OK, intent)
+                    finish()
+                } else {
+                    // 여행계획 수정
+                }
+           }
+        }
     }
+
 
 }
--- app/src/main/java/com/duran/travelapp/travelPlan/TravelPlanFragment.kt
+++ app/src/main/java/com/duran/travelapp/travelPlan/TravelPlanFragment.kt
@@ -1,30 +1,45 @@
 
 package com.duran.travelapp.travelPlan
 
+import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
 import com.duran.travelapp.R
 import com.duran.travelapp.addPlan.AddPlanActivity
 import com.duran.travelapp.databinding.FragmentTravelPlanBinding
+import com.duran.travelapp.dto.AddPlanDto
+import com.duran.travelapp.viewmodel.AddPlanViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class TravelPlanFragment : Fragment() {
 
     private lateinit var hBinding: FragmentTravelPlanBinding
+
+    lateinit var addPlanViewModel: AddPlanViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_travel_plan, container, false)
+        val view =  inflater.inflate(R.layout.fragment_travel_plan, container, false)
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        addPlanViewModel = ViewModelProvider(this)[AddPlanViewModel::class.java]
 
         val fragmentTravelPlanBinding = FragmentTravelPlanBinding.bind(view)
 
@@ -32,11 +47,28 @@ class TravelPlanFragment : Fragment() {
 
         fragmentTravelPlanBinding.btnAddTravelPlan.setOnClickListener {
             context?.let {
-                val intent = Intent(it, AddPlanActivity::class.java)
-                startActivity(intent)
+                val intent = Intent(it, AddPlanActivity::class.java).apply {
+                    putExtra("type", "ADD")
+                }
+                requestActivity.launch(intent)
+                //startActivity(intent)
             }
         }
 
+    }
+
+    private val requestActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if(it.resultCode == RESULT_OK){
+            val addPlan = it.data?.getSerializableExtra("addPlan") as AddPlanDto
+
+            when(it.data?.getIntExtra("flag", -1)) {
+                0 -> {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        addPlanViewModel.insert(addPlan)
+                    }
+                }
+            }
+        }
     }
 
 }
